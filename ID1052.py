907d1a61154e4cbdb9dfc2e55062901aef9fb1a9
LeoYHZ <34305621+LeoYHZ@users.noreply.github.com>
2021-11-21 18:11:41 +0800

    21-11-21

--- ID1052.py
+++ ID1052.py
@@ -4,9 +4,14 @@ input_num = int(input())
 num_list = list(map(int, input().split()))
 
 num_inversion = 0
-for i in range(input_num - 1):
-    for j in range(i + 1,input_num):
-        if (num_list[i] > num_list[j]):
-            num_inversion += 1
+# for i in range(input_num - 1):
+#     for j in range(i + 1,input_num):
+#         if (num_list[i] > num_list[j]):
+#             num_inversion += 1
+
+flag_list = [0]*11
+for i in range(input_num):
+    flag_list[num_list[i]] = flag_list[num_list[i]] + 1
+    num_inversion += sum(flag_list[(num_list[i] + 1):])
 
 print(num_inversion)
